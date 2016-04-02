61cef353baeba5bf8fa5918bbfd93849d08225fd
Mofo Admin <emccullough@modo-mbp292-emccullough.local>
2016-04-02 00:43:45 -0400

    Initial Commit

---
+++ Pacman Maze Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pacman Maze Game
+//
+//  Created by Evan McCullough on 4/2/16.
+//  Copyright © 2016 Evan McCullough. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
